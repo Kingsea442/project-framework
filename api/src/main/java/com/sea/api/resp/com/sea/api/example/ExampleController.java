664510a05f34df4310ef33ae40500987350d446b
wanglh <wanglh7@163.com>
2020-06-27 17:27:11 +0800

    fix ResponseBodyRewriter

--- api/src/main/java/com/sea/api/resp/com/sea/api/example/ExampleController.java
+++ api/src/main/java/com/sea/api/resp/com/sea/api/example/ExampleController.java
@@ -1,21 +1,24 @@
 package com.sea.api.resp.com.sea.api.example;
 
 import com.google.common.collect.Lists;
-import com.sea.api.resp.ApiResp;
-import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by wanglh on 2020/6/16.
  */
 @RestController
 @RequestMapping(value = "/api/example")
+@Slf4j
 public class ExampleController {
 
   @GetMapping(value = "/response/body/rewrite")
   public List<String> testResponseBodyRewrite() {
+    log.info("first slf4j log test :{}", "hello world");
     return Lists.newArrayList("hello", "world");
   }
 }
