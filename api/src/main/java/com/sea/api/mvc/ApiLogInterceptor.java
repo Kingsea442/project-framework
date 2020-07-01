6db400d60f6bc9ae6bdf6547d32dc7bb8f1459a8
wanglh <lianhai@preferr.com>
2020-07-01 15:49:24 +0800

    代码小调整

--- api/src/main/java/com/sea/api/mvc/ApiLogInterceptor.java
+++ api/src/main/java/com/sea/api/mvc/ApiLogInterceptor.java
@@ -1,7 +1,6 @@
 package com.sea.api.mvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.base.Preconditions;
 import com.sea.common.util.RequestUtil;
@@ -11,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
